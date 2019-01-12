dd40e804d8ec1363d2cfabeff852b4e884f2ee25
Roman Kirpichnikov <roman.kirpichnikov@binomo.com>
2019-01-12 22:43:00 +0300

    dockerize enviroment

---
+++ app.dockerfile
@@ -0,0 +1,42 @@
+FROM ruby:2.5
+LABEL Stanislav Mekhonoshin <ejabberd@gmail.com>
+
+ARG secret_token
+ENV SECRET_TOKEN=$secret_token
+
+RUN apt-get update && apt-get install -y --no-install-recommends \
+  nodejs \
+  openjdk-8-jre-headless \
+  xvfb \
+  libxi6 \
+  libgconf-2-4 \
+  netcat \
+  wget \
+  unzip
+
+RUN curl -sS -o - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add \
+  && echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list \
+  && apt-get -y update \
+  && apt-get -y install google-chrome-stable \
+  && rm -rf /var/lib/apt/lists/* /var/cache/apt/*
+
+RUN wget -N http://chromedriver.storage.googleapis.com/$(curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE)/chromedriver_linux64.zip -P ~/ \
+  && unzip ~/chromedriver_linux64.zip -d ~/ \
+  && rm ~/chromedriver_linux64.zip \
+  && mv -f ~/chromedriver /usr/local/bin/chromedriver \
+  && chown root:root /usr/local/bin/chromedriver \
+  && chmod 0755 /usr/local/bin/chromedriver
+
+RUN wget -N http://selenium-release.storage.googleapis.com/3.9/selenium-server-standalone-3.9.0.jar -P ~/ \
+  && mv -f ~/selenium-server-standalone-3.9.0.jar /usr/local/bin/selenium-server-standalone.jar \
+  && chown root:root /usr/local/bin/selenium-server-standalone.jar \
+  && chmod 0755 /usr/local/bin/selenium-server-standalone.jar
+
+WORKDIR /app
+COPY . .
+RUN cp config/database.yml.sample config/database.yml
+
+RUN gem install foreman
+RUN bundle install
+
+RUN rake assets:precompile
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,59 +1,73 @@
+# frozen_string_literal: true
+
 require 'simplecov'
 SimpleCov.start
 
-ENV["RAILS_ENV"] ||= 'test'
-require File.expand_path("../../config/environment", __FILE__)
+ENV['RAILS_ENV'] ||= 'test'
+require File.expand_path('../config/environment', __dir__)
 require 'rspec/rails'
 require 'sidekiq/testing'
 require 'capybara/rails'
 require 'capybara/rspec'
 require 'capybara/email/rspec'
 require 'shoulda-matchers'
-Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
-Dir[Rails.root.join("app/helpers/**/*.rb")].each {|f| require f}
-Dir[Rails.root.join("spec/shared_examples/*.rb")].each {|f| require f}
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+Dir[Rails.root.join('app/helpers/**/*.rb')].each { |f| require f }
+Dir[Rails.root.join('spec/shared_examples/*.rb')].each { |f| require f }
 
-Capybara.javascript_driver = :selenium_chrome_headless
+Capybara.server = :puma, { Silent: true }
+Capybara.register_driver :chrome_headless do |app|
+  options = ::Selenium::WebDriver::Chrome::Options.new
+
+  options.add_argument('--headless')
+  options.add_argument('--no-sandbox')
+  options.add_argument('--disable-gpu')
+
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
+end
+
+Capybara.javascript_driver = :chrome_headless
+
 Zonebie.set_random_timezone
 FakeWeb.allow_net_connect = false
 ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
 
 RSpec.configure do |config|
   config.expect_with :rspec do |c|
-    c.syntax = [:should, :expect]
+    c.syntax = %i[should expect]
   end
   config.mock_with :mocha
 
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.use_transactional_fixtures = false
   config.infer_base_class_for_anonymous_controllers = false
-  config.order = "random"
+  config.order = 'random'
   config.infer_spec_type_from_file_location!
 
-  config.include ApplicationHelper, :type => :feature
-  config.include Devise::Test::ControllerHelpers, :type => :controller
+  config.include ApplicationHelper, type: :feature
+  config.include Devise::Test::ControllerHelpers, type: :controller
   config.include FactoryGirl::Syntax::Methods
-  config.include Requests::JsonHelpers, :type => :controller
-  config.extend  Requests::ControllerMacros, :type => :controller
+  config.include Requests::JsonHelpers, type: :controller
+  config.extend  Requests::ControllerMacros, type: :controller
 
   config.before(:suite) do
     DatabaseCleaner.strategy = :transaction
   end
 
   config.before(:each) do |example|
-    if example.metadata[:disable_transaction]
-      DatabaseCleaner.strategy = :truncation
-    else
-      DatabaseCleaner.strategy = :transaction
-    end
+    DatabaseCleaner.strategy = if example.metadata[:disable_transaction]
+                                 :truncation
+                               else
+                                 :transaction
+                               end
     DatabaseCleaner.start
   end
 
   config.before(:each, type: :feature) do
     # Для капибарных тестов заполним редис курсами, чтобы не пытался стянуть их из инета
     redis = Redis.new
-    redis.set("smartvpn:eur_usd", 10)
-    redis.set("smartvpn:rub_usd", 10)
+    redis.set('smartvpn:eur_usd', 10)
+    redis.set('smartvpn:rub_usd', 10)
 
     FakeWeb.allow_net_connect = true
   end
