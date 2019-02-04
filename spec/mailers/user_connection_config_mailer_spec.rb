8f5c2ac46222b30e5c14698cafbda5e8fcca1373
Stanislav Mekhonoshin <ejabberd@gmail.com>
2019-02-04 18:36:19 +0000

    Send vpn credentials instead of UI on manual invitation.

--- spec/mailers/user_connection_config_mailer_spec.rb
+++ spec/mailers/user_connection_config_mailer_spec.rb
@@ -4,12 +4,11 @@ require 'rails_helper'
 
 describe UserConnectionConfigMailer do
   describe '#notify' do
-    subject { described_class.notify(user: user, crypted_password: crypted_password) }
+    subject { described_class.notify(user: user) }
     let!(:user) { create(:user) }
     let!(:server) { create(:server) }
     let!(:plan) { create(:plan, users: [user], servers: [server]) }
     let(:server_config) { ServerConfigBuilder.new(server: server).to_text }
-    let(:crypted_password) { Base64.encode64('123456') }
 
     its(:subject) { is_expected.to eq I18n.t('mailers.user_connection_config_mailer.subject') }
     its(:to) { is_expected.to eq [user.email] }
@@ -23,8 +22,12 @@ describe UserConnectionConfigMailer do
       expect(subject.attachments.first.filename).to eq "#{server.hostname}.ovpn"
     end
 
-    it 'renders user password' do
-      expect(subject.body.encoded).to match('123456')
+    it 'renders vpn username' do
+      expect(subject.body.encoded).to match(user.vpn_login)
+    end
+
+    it 'renders vpn password' do
+      expect(subject.body.encoded).to match(user.vpn_password)
     end
   end
 end
