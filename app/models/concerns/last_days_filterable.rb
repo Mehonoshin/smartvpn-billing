b1365670a5227b1dcffcc8928852dd996460190f
araslanov_e <araslanov_e_i@list.ru>
2019-02-19 16:25:35 +0500

    Added cop to travis

--- app/models/concerns/last_days_filterable.rb
+++ app/models/concerns/last_days_filterable.rb
@@ -1,9 +1,14 @@
+# frozen_string_literal: true
+
 module LastDaysFilterable
   extend ActiveSupport::Concern
 
   included do
     def self.in_days(number_of_days)
-      where(created_at: [number_of_days.days.ago.to_time.beginning_of_day..Date.current.tomorrow.to_time.end_of_day])
+      from_at = number_of_days.days.ago.to_time.beginning_of_day
+      to_at = Date.current.tomorrow.to_time.end_of_day
+
+      where(created_at: [from_at..to_at])
     end
   end
 end
