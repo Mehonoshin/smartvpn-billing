8b755f1ba7fa00926b6fa475c1fb0fcec4277141
Ilya Erin <ilya@erinphoto.ru>
2019-02-19 22:28:48 +0300

    Added and setup clockwork, moved scheduled rake tasks to sidekiq workers

--- spec/config/clock_spec.rb
+++ spec/config/clock_spec.rb
@@ -1,8 +1,15 @@
 # frozen_string_literal: true
 
+require 'rails_helper'
 require 'clockwork/test'
 
 describe Clockwork do
+  before do
+    allow(WithdrawalsWorker).to receive(:perform_async).and_return('WithdrawalsID')
+    allow(UpdateCoursesWorker).to receive(:perform_async).and_return('UpdateCoursesID')
+    allow(RefreshProxyListWorker).to receive(:perform_async).and_return('RefreshProxyListID')
+  end
+
   after(:each) { Clockwork::Test.clear! }
 
   %w[Withdrawals UpdateCourses RefreshProxyList].each do |job|
@@ -11,7 +18,7 @@ describe Clockwork do
 
       expect(Clockwork::Test.ran_job?(job)).to be_truthy
       expect(Clockwork::Test.times_run(job)).to eq 1
-      expect(Clockwork::Test.block_for(job).call).to be_kind_of(String)
+      expect(Clockwork::Test.block_for(job).call).to eq("#{job}ID")
     end
 
     it "runs the job #{job} every hour" do
@@ -22,5 +29,23 @@ describe Clockwork do
 
       expect(Clockwork::Test.times_run(job)).to eq(24)
     end
+  end
+
+  it 'executes correct Sidekiq worker for Withdrawals task' do
+    Clockwork::Test.run(max_ticks: 1)
+    Clockwork::Test.block_for('Withdrawals').call
+    expect(WithdrawalsWorker).to have_received(:perform_async).once
+  end
+
+  it 'executes correct Sidekiq worker for UpdateCourses task' do
+    Clockwork::Test.run(max_ticks: 1)
+    Clockwork::Test.block_for('UpdateCourses').call
+    expect(UpdateCoursesWorker).to have_received(:perform_async).once
+  end
+
+  it 'executes correct Sidekiq worker for RefreshProxyList task' do
+    Clockwork::Test.run(max_ticks: 1)
+    Clockwork::Test.block_for('RefreshProxyList').call
+    expect(RefreshProxyListWorker).to have_received(:perform_async).once
   end
 end
