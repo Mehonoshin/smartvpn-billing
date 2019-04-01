44ac32aaf542ef17c042ef5d44c20b5f636480b1
Stanislav Mekhonoshin <ejabberd@gmail.com>
2019-04-01 23:31:15 +0300

    Prevent popping up emails in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,6 @@
+# To prevent popping up email notifications from seeds we off
+ActionMailer::Base.perform_deliveries = false
+
 Dir[File.join(Rails.root, 'db', 'seeds', '*.rb')].sort.each do |seed|
   load seed
 end
