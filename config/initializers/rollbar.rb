080468c42d7d9ca491f4ad2e6087e287dbcb22da
Stanislav Mekhonoshin <ejabberd@gmail.com>
2019-04-01 23:07:38 +0300

    Disable rollbar on empty token

--- config/initializers/rollbar.rb
+++ config/initializers/rollbar.rb
@@ -8,7 +8,7 @@ Rollbar.configure do |config|
   # Without configuration, Rollbar is enabled by in all environments.
   # To disable in specific environments, set config.enabled=false.
   # Here we'll disable in 'test':
-  config.enabled = false if Rails.env.test? || Rails.env.development?
+  config.enabled = false if Rails.env.test? || Rails.env.development? || ENV['ROLLBAR_ACCESS_TOKEN'].nil?
 
   # By default, Rollbar will try to call the `current_user` controller method
   # to fetch the logged-in user object, and then call that object's `id`,
